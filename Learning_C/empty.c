d6d7e2df383f18269b308ead357a0732f9cde9e8
Steven Fuller <65366364+sfulle14@users.noreply.github.com>
2023-05-06 10:35:07 -0500

    add comments

--- Learning_C/empty.c
+++ Learning_C/empty.c
@@ -2,6 +2,11 @@
 
 int main()
 {
+    //This is how you single line comment in C.
+    /*
+    This is how you multi line comment in C.
+    */
+    //This code prints out a string.
     puts("Challenge: Add a puts() statement");
     puts("Print to screen");
     
