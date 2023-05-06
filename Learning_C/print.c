244c8b948de92b8ff00dcdff6e68ea207994c52f
Steven Fuller <65366364+sfulle14@users.noreply.github.com>
2023-05-06 10:39:24 -0500

    add printf

--- Learning_C/print.c
+++ Learning_C/print.c
@@ -8,8 +8,10 @@ int main()
     */
     //This code prints out a string.
     puts("Challenge: Add a puts() statement");
-    puts("Print to screen");
+    puts("Print to screen\n");
     
+    printf("This is another way to print using printf. ");
+    printf("Printf does not add a new line on its own so needs \\n \n");
 
     return(0);
 }
