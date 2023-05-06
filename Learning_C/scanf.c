f34fb884f9134d83662dbf14c85f0ffa602212e8
Steven Fuller <65366364+sfulle14@users.noreply.github.com>
2023-05-06 11:08:33 -0500

    add float and char

--- Learning_C/scanf.c
+++ Learning_C/scanf.c
@@ -4,6 +4,7 @@ int main()
 {
     int x;
     float y;
+    char z;
 
     printf("Type an integer: ");
     scanf("%d",&x);  //this is how you get user input!
@@ -12,6 +13,10 @@ int main()
     printf("Type any number: ");
     scanf("%f",&y);  //this is how you get user input!
     printf("Integer %f\n",y);
+
+    printf("Type a character: ");
+    scanf("%c",&z);  //this is how you get user input!
+    printf("Character %c\n",z);
 
     return(0);
 }
