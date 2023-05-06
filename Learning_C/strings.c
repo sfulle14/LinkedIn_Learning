81b4bad9bccad77e05c05d4b61f0da7691005571
Steven Fuller <65366364+sfulle14@users.noreply.github.com>
2023-05-06 11:14:23 -0500

    add fgets

--- Learning_C/strings.c
+++ Learning_C/strings.c
@@ -3,8 +3,14 @@
 int main() 
 {
     char password[] = "password";\
+    char input[64];
 
     printf("The password is %s.\n",password);
+
+    printf("Instructions: ");
+    fgets(input, 64, stdin);    //gets a line of text
+    puts("Thank you! Here are your instructions: ");
+    puts(input);
 
     return(0);
 }
