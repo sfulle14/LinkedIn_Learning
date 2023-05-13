2e4d7556fe83bf5833c939365da7b2bd5c3d4bdc
Steven Fuller <65366364+sfulle14@users.noreply.github.com>
2023-05-13 16:03:15 -0500

    loops and functions

---
+++ Learning_C/functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void blorf(void);
+
+int main()
+{
+    puts("The main() function always runs first.");
+    blorf();
+    
+    return(0);
+}
+
+void blorf(void){
+    puts("The blorf() function runs when it's called.");
+}
