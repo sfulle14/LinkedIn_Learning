c1a6d542236ac770eb0bc83d75cd1d43c5334bb5
Steven Fuller <65366364+sfulle14@users.noreply.github.com>
2023-05-13 16:08:26 -0500

    added another function

--- Learning_C/functions.c
+++ Learning_C/functions.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
 void blorf(void);
+void repeat(int count);
 
 int main()
 {
-    puts("The main() function always runs first.");
-    blorf();
-    
+    //puts("The main() function always runs first.");
+    //blorf();
+
+    int a,b;
+
+    a=1;
+    b=5;
+
+    puts("At first the raven was like:");
+    repeat(a);
+    puts("But then he was all:");
+    repeat(b);
+
     return(0);
+}
+
+void repeat(int count){
+    int x;
+
+    for(x=0; x<count; x++){
+        puts("Nevermore!");
+    }
 }
 
 void blorf(void){
